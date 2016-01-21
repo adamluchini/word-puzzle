31efab15c6bf30406c0374f04e3ce97b3abad311
Joseph Karasek and Steve Kinney <skinney103@gmail.com>
2016-01-21 10:47:02 -0800

    add function wordPuzzle, replaces one character strings

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// create a function
+var wordPuzzle = function(inputString) {
+  if(inputString[0]==="a") {
+    inputString = "-";
+  }
+  // it will check if caracter is a vowel
+  // change vowels to -
+  // function to return a string, inputString
+  return inputString;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe("wordPuzzle", function() {
     it("replaces vowels with a dash", function() {
-      expect(wordPuzzle("cat")).to.equal("c-t");  
+      expect(wordPuzzle("cat")).to.equal("c-t");
+    });
+    it("replaces a single vowel with a dash", function() {
+      expect(wordPuzzle("a")).to.equal("-");  
+    });
+    it("does not replace consonant with a dash", function() {
+      expect(wordPuzzle("c")).to.equal("c");
     });
 });

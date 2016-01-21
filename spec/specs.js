afa072190339cde670c0d4772df9c4d2e2c48bd7
Joseph Karasek and Steve Kinney <skinney103@gmail.com>
2016-01-21 11:34:59 -0800

    add spec, replace all vowels with a -

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,14 @@
 describe("wordPuzzle", function() {
-    it("replaces vowels with a dash", function() {
-      expect(wordPuzzle("cat")).to.equal("c-t");
-    });
-    it("replaces a single vowel with a dash", function() {
-      expect(wordPuzzle("a")).to.equal("-");  
-    });
-    it("does not replace consonant with a dash", function() {
-      expect(wordPuzzle("c")).to.equal("c");
-    });
+  it("replaces vowels with a dash", function() {
+    expect(wordPuzzle("cat")).to.equal("c-t");
+  });
+  it("replaces a single vowel with a dash", function() {
+    expect(wordPuzzle("a")).to.equal("-");
+  });
+  it("does not replace consonant with a dash", function() {
+    expect(wordPuzzle("c")).to.equal("c");
+  });
+  it("relpace all vowels with a -", function() {
+    expect(wordPuzzle("the cat is furry, meow")).to.equal("th- c-t -s f-rry, m--w");
+  });
 });
